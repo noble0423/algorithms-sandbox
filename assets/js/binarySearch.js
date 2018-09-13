4d10dc9b178443832076d1b0587e466f44a64dd0
noble0423 <ryan.noble82@gmail.com>
2018-09-13 15:23:50 -0500

    countNums() and simpleSearch() are working properly.

--- assets/js/binarySearch.js
+++ assets/js/binarySearch.js
@@ -1,16 +1,33 @@
 // VARIABLES 
 //============================================================================================
-
+var count = 0;
+var numArray = [2, 6, 3, 9, 1, 0, 7, 5, 8, 4];
+var searchNum = 0;
 
 
 // FUNCTIONS
 //============================================================================================
+function countNums() {
+    for (let i = 0; i < numArray.length; i++) {
+        count++
+    };
+    console.log("There are " + count + " total numbers in the array.");
+};
 
+function simpleSearch() {
+    for (let i = 0; i < numArray.length; i++) {
+        if (numArray[i] === searchNum) {
+            console.log("found " + searchNum + " at index " + numArray.indexOf(searchNum));
+        }
+    };
+};
 
 // MAIN PROCESS
 //============================================================================================
 $(document).ready(function() {
     $("#test-button").on("click", function() {
-        console.log("clicked");
+        // console.log("clicked");
+        countNums();
+        simpleSearch();
     })
 });
